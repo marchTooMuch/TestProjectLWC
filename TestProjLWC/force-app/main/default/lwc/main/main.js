97b32310a077b389f49a252f37be0669846b3ef1
abcd <markrybnikov2000@gmail.com>
2021-04-20 11:19:01 +0300

    Last version of project

--- TestProjLWC/force-app/main/default/lwc/main/main.js
+++ TestProjLWC/force-app/main/default/lwc/main/main.js
@@ -1,41 +1,89 @@
-
-import { LightningElement} from 'lwc';
-
+import { LightningElement,track,wire,api } from 'lwc';
+import getContacts from '@salesforce/apex/Contacts.getContacts';
+import { createMessageContext, releaseMessageContext, publish, subscribe, unsubscribe  } from 'lightning/messageService';
+import DragDropChannel from "@salesforce/messageChannel/DragDrop__c";
 export default class Main extends LightningElement {
-  
-  handleClick(event) {
-    var element = this.template.querySelector('#' +event.target.id);
-
-    element.parentNode.remove();
-}
-
-    drag(event){
-        event.dataTransfer.setData("divId", event.target.id);
-        
+    @track contacts2 = [];
+    draggedItem;
+    draggedItemId;
+    context = createMessageContext();
+    subscription = null;
+    @track lstPosts1 = [];
+    @track lstPosts2 = [];
+    constructor() {
+        super();
+        getContacts()
+            .then(result => {
+                this.contacts2 = result;
+            })
+            .catch(error => {
+                this.error = error;
+            });
+        if (this.subscription) { 
+            return;
+        }
+        this.subscription = subscribe(this.context, DragDropChannel, (message) => {
+            if(message.upcomingToCompleted){
+                this.handleMessage(message);
+            } else if(!message.upcomingToCompleted){
+                this.template.querySelector('#'+message.itemId).remove();
+            }
+        });
     }
-
+    handleMessage(message) {
+        this.draggedItem = message.draggedItem;
+        this.draggedItemId = message.itemId;
+    }
     allowDrop(event){
         event.preventDefault();
     }
-
-    drop(event){
+    drop(event) {
         event.preventDefault();
-        var divId = event.dataTransfer.getData("divId");
-        var draggedElement = this.template.querySelector('#' +divId);
-        draggedElement.classList.add('completed');
-        event.target.appendChild(draggedElement);
-        
+        if(event.target.id ==="idCompletedPosts-64") {
+            this.lstPosts2.push(this.draggedItem);
+            const message = {
+            draggedItem : null,
+            itemId : this.draggedItemId,
+            upcomingToCompleted: false
+            } 
+            publish(this.context, DragDropChannel, message);
+        } else if(event.target.id ==="idUpcomingPosts-64") {
+            this.lstPosts1.push(this.draggedItem);
+            const message = {
+            draggedItem : null,
+            itemId : this.draggedItemId,
+            upcomingToCompleted: false
+            } 
+            publish(this.context, DragDropChannel, message);
+        }
+    }
+    drag(event) {
+        const message = {
+            draggedItem : event.target.textContent,
+            itemId : event.target.id,
+            upcomingToCompleted: true
+        };
+        publish(this.context, DragDropChannel, message);
+    }
+    disconnectedCallback() {
+        releaseMessageContext(this.context);
+    }
+    handleClick(event) {
+        var element = event.target;
+       element.parentNode.remove();
+    }
+    handleClick2(event) {
+        this.contacts2.forEach(element=>{this.lstPosts1.push(element.Name)});
+        this.contacts2.forEach(element=>{this.lstPosts2.push(element.Phone)});
+    }
+    get makeId() {
+        var result           = [];
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqr';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < 8; i++ ) {
+            result.push(characters.charAt(Math.floor(Math.random() * 
+       charactersLength)));
+         }
+         return result.join('');
     }
 }
-  // initialize component
-  // renderedCallback() {
-  //   var p1 = document.createElement("p");
-  //   var node1 = document.createTextNode("Hi");
-  //   var p2 = document.createElement("p");
-  //   var node2 = document.createTextNode("Hi");
-  //   p1.appendChild(node1);
-  //   p2.appendChild(node2);
-  //   var div = this.template.querySelectorAll('div')
-  //   div[0].appendChild(p1);
-  //   div[1].appendChild(p2);
-  // }
